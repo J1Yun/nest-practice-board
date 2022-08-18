004d60c962503c720bb80aeb7948c729c4e4e405
Choi-Ji-Yun <cjy5025@naver.com>
2022-08-19 01:01:15 +0900

    DB를 활용한 CRUD 구현
    - 특정 게시물 조회
    - 게시물 생성
    - 게시물 삭제
    - 게시물 상테 업데이트

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Board } from './boards.entity';
+import { BoardStatus } from './boards.enum';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  // 특정 게시물 조회
+  @Get('/:id')
+  getBoardById(@Param('id') id: number): Promise<Board> {
+    return this.boardsService.getBoardById(id);
+  }
+
+  // 게시물 생성
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  // 게시물 삭제
+  @Delete('/:id')
+  deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
+    return this.boardsService.deleteBoard(id);
+  }
+
+  // 게시물 상태 업데이트
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status') status: BoardStatus,
+  ): Promise<Board> {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Board } from './boards.entity';
+import { BoardStatus } from './boards.enum';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  constructor(
+    @InjectRepository(Board) private boardRepository: Repository<Board>,
+  ) {}
+
+  // 특정 게시물 조회
+  async getBoardById(id: number): Promise<Board> {
+    const board = await this.boardRepository.findOne({ where: { id } });
+
+    if (!board) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return board;
+  }
+
+  // 게시물 생성
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const { title, description } = createBoardDto;
+
+    const board = this.boardRepository.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.boardRepository.save(board);
+    return board;
+  }
+
+  // 게시물 삭제
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+  }
+
+  // 게시물 상태 업데이트
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.getBoardById(id);
+
+    board.status = status;
+    await this.boardRepository.save(board);
+
+    return board;
+  }
+}
